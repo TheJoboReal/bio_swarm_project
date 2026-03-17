4266ab472659ae6adfe2510d23dd2b1fa422042b
Aksel_u24 <akmoe22@student.sdu.dk>
2026-03-17 15:42:37 +0100

    progess so far

--- main.py
+++ main.py
@@ -5,14 +5,21 @@ from matplotlib.animation import FuncAnimation
 
 ARENA_SIDE_LENGTH = 20
 NUMBER_OF_ROBOTS = 30
-STEPS = 1200
+STEPS = 1#1200
+FPS = 60
 MAX_SPEED = 0.1
 
 
 ### Initiate random robots with random positions and velocities.
 # Positions
-x = np.random.uniform(low=0, high=ARENA_SIDE_LENGTH, size=(NUMBER_OF_ROBOTS,))
-y = np.random.uniform(low=0, high=ARENA_SIDE_LENGTH, size=(NUMBER_OF_ROBOTS,))
+# x = np.random.uniform(low=0, high=ARENA_SIDE_LENGTH, size=(NUMBER_OF_ROBOTS,))
+# y = np.random.uniform(low=0, high=ARENA_SIDE_LENGTH, size=(NUMBER_OF_ROBOTS,))
+x = np.random.uniform(low=ARENA_SIDE_LENGTH/4, high=3*ARENA_SIDE_LENGTH/4, size=(NUMBER_OF_ROBOTS,)) #Initiate agents in the "middle" of arena
+y = np.random.uniform(low=ARENA_SIDE_LENGTH/4, high=3*ARENA_SIDE_LENGTH/4, size=(NUMBER_OF_ROBOTS,)) #Initiate agents in the "middle" of arena
+
+#Remember initial position for each agent
+initial_position_x = x
+initial_position_y = y
 
 # Velocities
 vx = np.random.uniform(low=-MAX_SPEED, high=MAX_SPEED, size=(NUMBER_OF_ROBOTS,))
@@ -46,93 +53,24 @@ def distance_between_agents(i, j):
     return distance
 
 
-#################### COHESION ####################
-#kilde: https://keyirobot.com/blogs/buying-guide/exploring-swarm-robotics-programming-multiple-simple-agents
-#Kilde2: https://medium.com/better-programming/boids-simulating-birds-flock-behavior-in-python-9fff99375118
-def cohesion(i, min_distanceThreshold_for_cohesion_bedreNavnErEnGodIde): #tiltrækning til naboer
-    # For every robot/agent/boid (find et navn) we need to find the average x and y position of its neighbors
-    avg_x = 0
-    avg_y = 0
-    denominator_in_avg_calculation = 0.0
 
-    # Loop through the robots and find avg
+
+def cohesion_separation(i, sensor_range, delta):
+    #Count number of neighbors
+    neighbor_count = 0
     for j in range(NUMBER_OF_ROBOTS):
-        if i != j: #ensure different neighbor
+        if i != j:
 
-            #Check that distnace to neighbor j is within threshold
-            distance = distance_between_agents(i, j)
-            if distance < min_distanceThreshold_for_cohesion_bedreNavnErEnGodIde:
-                avg_x += x[j] # ligger nabos position til avg_x
-                avg_y += y[j] # og for y også
-                denominator_in_avg_calculation += 1 #skal bruges til at dividere med for at få gennemsnit
+            distance = distance_between_agents(i,j)
+            if distance < sensor_range:
+                neighbor_count += 1
 
-    # Find average by dividing by number of neigbors
-    if denominator_in_avg_calculation > 0:
-        avg_x = avg_x / denominator_in_avg_calculation
-        avg_y = avg_y / denominator_in_avg_calculation
-        return avg_x - x[i], avg_y - y[i] 
-
-    #Does nothing if no neighbor
-    return 0.0, 0.0
-
-
-
-#################### SEPARATION ####################
-#kilde: https://keyirobot.com/blogs/buying-guide/exploring-swarm-robotics-programming-multiple-simple-agents
-def separation(i, min_seperation_distance_threshold):
-    # Which direction the agents will move according to seperation
-    sx = 0.0
-    sy = 0.0
-    
-    #Loop through nearby (given by threshold) neighbors
     for j in range(NUMBER_OF_ROBOTS):
-        if i != j:#ensure different neighbor
+        if i != j:
+            cohesion_separation_gain = i - delta * neighbor_count / np.sqrt(x[i]**2 + y[i]**2)
 
-            #Check that distnace to neighbor j is within threshold
-            distance = distance_between_agents(i, j)
-            if distance < min_seperation_distance_threshold:
-                #Calc difference betweeen agent i and -j
-                dx = x[j] - x[i]
-                dy = y[j] - y[i]
-
-                sx -= dx #minus ensures agent moves away from other agents -> repels neighbors
-                sy -= dy
-                
-    return sx, sy
-
-
-#################### ALIGNMENT ####################
-#kilde: https://keyirobot.com/blogs/buying-guide/exploring-swarm-robotics-programming-multiple-simple-agents
-def alignment_Velocity_Based(i, min_alligment_distance_threshold):
-    #variables for avg speed
-    avg_vx = 0.0
-    avg_vy = 0.0
-    denominator_in_avg_calc = 0.0
-
-    #  Find average speed of neighbors
-    for j in range(NUMBER_OF_ROBOTS):
-        if j != i: #ensure different neighbor
-            
-            #Check that distnace to neighbor j is within threshold
-            distance = distance_between_agents(i, j)
-            if distance < min_alligment_distance_threshold:
-                avg_vx += vx[j]
-                avg_vy += vy[j]
-                denominator_in_avg_calc += 1
-    
-    # Find average by dividing by number of neigbors within given threshold
-    if denominator_in_avg_calc > 0:
-        avg_vx = avg_vx / denominator_in_avg_calc
-        avg_vy = avg_vy / denominator_in_avg_calc
-
-        #Fra kilde: Speed += allignment - current speed
-        allignment_output_vx = avg_vx - vx[i]
-        allignment_output_vy = avg_vy - vy[i]
-        return allignment_output_vx, allignment_output_vy
 
     return 0.0, 0.0
-
-
 
 
 def update(vx, vy):
@@ -141,20 +79,23 @@ def update(vx, vy):
     new_vy = np.copy(vy)
 
     #weights for controlling cohesion, separation and allignment
-    c_weight = 0.01 #0.01
-    s_weight = 0.05 #0.05
-    a_weight = 0.07 #0.03
+    cohesion_weight = 0.01 #0.01
+    separation_weight = 0.05 #0.05
+    alignment_weight = 0.07 #0.03
 
-    # Update each robot individually
+    # Update for each robot individually
     for i in range(NUMBER_OF_ROBOTS):
-        cx, cy = cohesion(i, 1.5); #print(cx, cy)
-        sx, sy = separation(i, 1.0)
-        ax, ay = alignment_Velocity_Based(i, 1.25)
+        cohesion_vx, cohesion_vy = cohesion_separation(i, sensor_range=4.0, delta=1.5)
 
 
-        #Update speed
-        new_vx[i] += c_weight*cx + s_weight*sx + a_weight*ax
-        new_vy[i] += c_weight*cy + s_weight*sy + a_weight*ay
+
+        #### Update speed...
+        #... from cohesion
+        new_vx[i] += cohesion_weight * cohesion_vx
+        new_vy[i] += cohesion_weight * cohesion_vy
+
+
+
 
         #Enforce max speed
         new_vx[i], new_vy[i] = enforce_max_speed_limit(new_vx[i], new_vy[i])
@@ -180,7 +121,8 @@ def animate(i):
     #Update step
     vx, vy = update(vx, vy)
 
-    
+    #Update position
+    dt = 1/FPS    
     x = wrap(x + vx)
     y = wrap(y + vy)
 
@@ -192,5 +134,5 @@ def animate(i):
 
 anim = FuncAnimation(fig, animate, init_func=init, frames=STEPS, interval=1, blit=True)
 
-videowriter = animation.FFMpegWriter(fps=60)
+videowriter = animation.FFMpegWriter(fps=FPS)
 anim.save("output.mp4", writer=videowriter)
