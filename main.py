a8836f4bc478d86da51a9a9bfc40e757f0a22d22
HBay22 <hniel22@student.sdu.dk>
2026-03-24 15:08:29 +0100

    added gamma

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 import cv2
 from agent import *
 
@@ -11,7 +13,41 @@ HEIGHT = 700
 WIDTH = 700
 
 
+# Calculate the gamma [-1,1] value that represents the drectional alignment. If gamma is approx 1, it indicates near-parallel velocities (strong alignment) and values near or below 0 indicat-ing misalignment. By normalizing direction, this metric isolates directional consensus from speed differences
+def directional_alignment(boid, flock, neighbor):
+    vx_i, vy_i = boid.get_velocity()
+    norm_vi = math.sqrt(vx_i**2 + vy_i**2)
+
+    if norm_vi == 0:
+        return 0
+
+    i = boid.get_id()
+
+    sum_alignment = 0.0
+    neighbor_count = 0
+
+    for j in neighbor:
+        if i == j:
+            continue
+
+        vx_j, vy_j = flock[j].get_velocity()
+        norm_vj = math.sqrt(vx_j**2 + vy_j**2)
+
+        if norm_vj == 0:
+            continue
+
+        dot = vx_i * vx_j + vy_i * vy_j
+        sum_alignment += dot / (norm_vi * norm_vj)
+        neighbor_count += 1
+
+    if neighbor_count == 0:
+        return 0
+
+    return sum_alignment / neighbor_count
+
+
 def distance_between_agents(boid_og, neighbor):
+
     x_og, y_og = boid_og.get_position()
     x_nb, y_nb = neighbor.get_position()
 
@@ -23,7 +59,6 @@ def distance_between_agents(boid_og, neighbor):
     return distance
 
 
-
 def cohesion_separation(boid, flock, sensor_range, delta):
     # Get index of current boid
     i = boid.get_id()
@@ -33,8 +68,8 @@ def cohesion_separation(boid, flock, sensor_range, delta):
     for j in range(NUMBER_OF_AGENTS):
         if i != j:
             distance = distance_between_agents(boid, flock[j])
-            if 0 < distance < sensor_range: # "0 <" to avoid dividing by zero later
-                neighbor_count += 1 #Count number of neighbors
+            if 0 < distance < sensor_range:  # "0 <" to avoid dividing by zero later
+                neighbor_count += 1  # Count number of neighbors
 
     # Calculate control input for boid i
     control_input_x, control_input_y = 0.0, 0.0
@@ -42,7 +77,6 @@ def cohesion_separation(boid, flock, sensor_range, delta):
         if i != j:
             distance = distance_between_agents(boid, flock[j])
             if 0 < distance < sensor_range:
-
                 # Calculate cohesion-seperation gain:
                 cohesion_separation_gain = 1 - (delta * neighbor_count) / distance
 
@@ -50,37 +84,38 @@ def cohesion_separation(boid, flock, sensor_range, delta):
                 boid_pos_x, boid_pos_y = boid.get_position()
                 neighbor_pos_x, neighbor_pos_y = flock[j].get_position()
 
-                control_input_x += cohesion_separation_gain * (neighbor_pos_x - boid_pos_x)
-                control_input_y += cohesion_separation_gain * (neighbor_pos_y - boid_pos_y)
-    #print(control_input_x, " ", control_input_y)
-    
+                control_input_x += cohesion_separation_gain * (
+                    neighbor_pos_x - boid_pos_x
+                )
+                control_input_y += cohesion_separation_gain * (
+                    neighbor_pos_y - boid_pos_y
+                )
+    # print(control_input_x, " ", control_input_y)
+
     return control_input_x, control_input_y
 
 
 def alignment_velocity_based(boid, flock, sensor_range):
-    #Current boid id and velocity
+    # Current boid id and velocity
     i = boid.get_id()
     boid_vx, boid_vy = boid.get_velocity()
 
-    #allignment variables
+    # allignment variables
     alignment_x, alignment_y = 0.0, 0.0
     for j in range(NUMBER_OF_AGENTS):
         if i != j:
             distance = distance_between_agents(boid, flock[j])
-            if 0 < distance < sensor_range: # "0 <" to avoid dividing by zero later
+            if 0 < distance < sensor_range:  # "0 <" to avoid dividing by zero later
                 neighbor_vel_x, neighbor_vel_y = flock[j].get_velocity()
                 alignment_x += neighbor_vel_x - boid_vx
                 alignment_y += neighbor_vel_y - boid_vy
-    
+
     return alignment_x, alignment_y
 
 
-
-
 def alignment_position_based(boid, flock, sensor_range, t):
-    #Current boid id
+    # Current boid id
     i = boid.get_id()
-
 
     sum_dx = 0.0
     sum_dy = 0.0
@@ -112,12 +147,11 @@ def alignment_position_based(boid, flock, sensor_range, t):
                 neighbor_count += 1
 
     if neighbor_count > 0 and t > 0:
-
         # Approx relative velocity by the long-term change in relative position
         alignment_output_vx = sum_dx / t
         alignment_output_vy = sum_dy / t
 
-        #print(alignment_output_vx, " " , alignment_output_vy)
+        # print(alignment_output_vx, " " , alignment_output_vy)
 
         print(t)
 
@@ -127,23 +161,18 @@ def alignment_position_based(boid, flock, sensor_range, t):
         alignment_output_vx = sum_dx / k
         alignment_output_vy = sum_dy / k
 
-        #print(alignment_output_vx, " " , alignment_output_vy)
+        # print(alignment_output_vx, " " , alignment_output_vy)
         return alignment_output_vx, alignment_output_vy
-        
 
     return 0.0, 0.0
 
 
+dt = 0.1  # ???
 
 
+def update(flock, t, gamma_t):
 
-
-
-dt = 0.1 #???
-
-def update(flock, t):
-    
-    t += dt 
+    t += dt
 
     # Update each boid individually
     for i in range(NUMBER_OF_AGENTS):
@@ -159,20 +188,34 @@ def update(flock, t):
         vy += dt * (cs_y + ay_pos_based)
 
         # speed limiter
-        speed = np.sqrt(vx**2 + vy**2)  
+        speed = np.sqrt(vx**2 + vy**2)
         if speed > MAX_SPEED:
             vx = (vx / speed) * MAX_SPEED
             vy = (vy / speed) * MAX_SPEED
 
-        #Execute update for boid i
+        # Execute update for boid i
         flock[i].update_velocity(vx, vy)
         flock[i].update_position()  # points.set_data(x, y)
 
-    return t
+    gamma_sum = 0.0
+    for i in range(len(flock)):
+        boid_i = flock[i]
+        gamma_sum += directional_alignment(boid_i, flock, neighbor=range(len(flock)))
+
+    gamma_value = gamma_sum / len(flock)
+    gamma_t.append(gamma_value)
+
+    print(t)
+    print(gamma_value)
+
 
 def main():
-    img = (np.ones((HEIGHT, WIDTH, 3), dtype=np.uint8) * 255)  # for at få en hvid baggrund
-    imgclear = (np.ones((HEIGHT, WIDTH, 3), dtype=np.uint8) * 255)  # for at få en hvid baggrund
+    img = (
+        np.ones((HEIGHT, WIDTH, 3), dtype=np.uint8) * 255
+    )  # for at få en hvid baggrund
+    imgclear = (
+        np.ones((HEIGHT, WIDTH, 3), dtype=np.uint8) * 255
+    )  # for at få en hvid baggrund
 
     blueColor = (255, 50, 50)
     agentRadius = 3
@@ -190,14 +233,18 @@ def main():
     for i in range(NUMBER_OF_AGENTS):
         flock.append(Boids(i, x[i], y[i], vx[i], vy[i], HEIGHT, WIDTH))
 
-    # Time variable
-    timeIterator = 0
+    # hold = False  # Flag to control the hold state
+    # gamma_t directional alignment values over time.
+    gamma_t = []
+
+    # Time passed
+    T = 0.1
 
     # Do the simulation
     while True:
         img = imgclear.copy()  # to clear the image
 
-        timeIterator += update(flock, timeIterator)
+        update(flock, T, gamma_t)
 
         for i in range(NUMBER_OF_AGENTS):
             xBoid, yBoid = flock[i].get_position()
@@ -221,6 +268,9 @@ def main():
         #     continue
 
     cv2.destroyAllWindows()
+
+    plt.plot(gamma_t)
+    plt.savefig("gamma_t_plot.png")
 
 
 ############ Main
--- todo.md
+++ todo.md
@@ -4,3 +4,4 @@
 - [] Implement the threshold and gain stuff from the paper.
 - [] Implement clustering metric gamma (page 8).
 - [] Distance function needs to take wrap-around into account.
+- [] Change speed limiter to saturation (page 4 bottom).
